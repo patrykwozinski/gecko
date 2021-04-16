dd30a4a932d717e48c016bbb5d105e1569b9e7d1
Patryk Woziński <patryk.wozinski@gmail.com>
2021-04-16 08:19:58 +0200

    cleanup defaults

--- lib/ex_gecko.ex
+++ lib/ex_gecko.ex
@@ -2,17 +2,4 @@ defmodule ExGecko do
   @moduledoc """
   Documentation for `ExGecko`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> ExGecko.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ex_gecko_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule ExGeckoTest do
-  use ExUnit.Case
-  doctest ExGecko
-
-  test "greets the world" do
-    assert ExGecko.hello() == :world
-  end
-end
